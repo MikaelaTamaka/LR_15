5ae5b361476c5dd3ad90b98c9935e56180fdb39b
MikaelaTamaka <144335602+MikaelaTamaka@users.noreply.github.com>
2024-05-02 09:42:04 +0300

    Add files via upload

---
+++ 151.pas
@@ -0,0 +1,59 @@
+﻿program AlphabetFrequencyDictionary;
+
+uses SysUtils, Classes, Generics.Collections;
+
+var
+  inputFile, outputFile: TextFile;
+  word: string;
+  freqDict: TDictionary<string, Integer>;
+  key: string;
+  count: Integer;
+
+begin
+  // Создание словаря для хранения частот слов
+  freqDict := TDictionary<string, Integer>.Create;
+
+  // Открываем входной файл
+  AssignFile(inputFile, 'C:\Users\Кампухтер\Desktop\лр15\input.txt');
+  Reset(inputFile);
+
+  // Считываем слова из файла и заполняем словарь
+  while not eof(inputFile) do
+  begin
+    Readln(inputFile, word);
+    if word <> '' then
+    begin
+      if freqDict.ContainsKey(word) then
+        freqDict[word] := freqDict[word] + 1
+      else
+        freqDict.Add(word, 1);
+    end;
+  end;
+
+  // Закрываем входной файл
+  CloseFile(inputFile);
+
+  // Сортируем словарь по ключам (словам)
+  for key in freqDict.Keys do
+  begin
+    freqDict.TryGetValue(key, count);
+    WriteLn(key, ' ', count);
+  end;
+
+  // Открываем файл для записи результатов
+  AssignFile(outputFile, 'output.txt');
+  Rewrite(outputFile);
+
+  // Записываем результаты в файл
+  for key in freqDict.Keys do
+  begin
+    freqDict.TryGetValue(key, count);
+    WriteLn(outputFile, key, ' ', count);
+  end;
+
+  // Закрываем выходной файл
+  CloseFile(outputFile);
+
+  // Освобождаем ресурсы
+  freqDict.Free;
+end.
---
+++ 153.pas
@@ -0,0 +1,58 @@
+﻿//Дан односвязный (линейный) список. Найти максимальный и минимальный элементы в списке.
+program LR153;
+type
+  TNode = record
+    Value: Integer;
+    Next: ^TNode;
+  end;
+
+var
+  Head, Current: ^TNode;
+  InputFile: TextFile;
+  Num, MaxValue, MinValue: Integer;
+
+begin
+  AssignFile(InputFile, 'input.txt');
+  Reset(InputFile);
+
+  New(Head);
+  Readln(InputFile, Num);
+  Head^.Value := Num;
+  Head^.Next := nil;
+  Current := Head;
+
+
+  while not Eof(InputFile) do
+  begin
+    New(Current^.Next);
+    Current := Current^.Next;
+    Readln(InputFile, Num);
+    Current^.Value := Num;
+    Current^.Next := nil;
+  end;
+
+  // Находим максимальный и минимальный элементы в списке
+  MaxValue := Head^.Value;
+  MinValue := Head^.Value;
+  Current := Head;
+  while Current <> nil do
+  begin
+    if Current^.Value > MaxValue then
+      MaxValue := Current^.Value;
+    if Current^.Value < MinValue then
+      MinValue := Current^.Value;
+    Current := Current^.Next;
+  end;
+
+  Writeln('max: ', MaxValue);
+  Writeln('min: ', MinValue);
+
+  CloseFile(InputFile);
+  Current := Head;
+  while Current <> nil do
+  begin
+    Current := Head^.Next;
+    Dispose(Head);
+    Head := Current;
+  end;
+end.
